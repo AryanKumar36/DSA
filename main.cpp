0830449318bdf13e60d152915280d60561494127
AryanKumar36 <ak24012000@gmail.com>
2023-08-29 11:52:45 +0530

    Stack Stl

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+
+int main()
+{
+
+    
+    //Creation of stack
+    stack<int> s;
+
+    //Push operation
+    s.push(2);
+    s.push(3);
+
+    //Pop operation
+    s.pop();
+
+    cout<<"Printing top element "<<s.top()<<endl;
+
+    if(s.empty())
+    {
+        cout<<"Stack is empty "<<endl;
+    }
+    else{
+        cout<<"Stack is not empty"<<endl;
+    }
+    
+    return 0;
+}
